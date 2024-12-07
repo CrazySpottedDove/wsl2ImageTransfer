2af9be26567aa54dda66c1ea19a9f6db8023623a
CrazySpottedDove <3230104178@zju.edu.cn>
2024-12-07 22:13:23 +0800

    添加了自动搜索截屏保存路径的功能，不需要用户自行配置。添加了开源证书。

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ vsce package
 
 ## Features
 
-使用 windows 自带的截图工具截图，然后使用 Ctrl + Alt + P 的快捷键插入图片。首次使用该插件，必须在插件设置中指定你的截屏图片保存路径。
+使用 windows 自带的截图工具截图，然后使用 Ctrl + Alt + P 的快捷键插入图片。首次使用该插件时，插件会尝试搜索你的 windows 注册表，以找到截屏图片的保存路径。如果搜索失败，可以必须自行指定。
 
 ## Requirements
 
@@ -20,7 +20,7 @@ vsce package
 
 ## Extension Settings
 
-* `wslImageTransfer.screenshotDir`: 你的 windows 主机的截图保存目录，默认为`C:/Users/<YourUsername>/Pictures/Screenshots`
+* `wslImageTransfer.screenshotDir`: 你的 windows 主机的截图保存目录(它默认为`C:/Users/<YourUsername>/Pictures/Screenshots`)
 * `wsl2ImageTransfer.targetDir`: 将要黏贴的图片在当前目录中的保存路径，默认为 `figures/`
 * `wsl2ImageTransfer.insertContent`: 将会在你的编辑器中插入的内容，默认为`\\begin{figure}[H]\n\\centering\n\\includegraphics[width=0.9\\textwidth]{${filePath}}\n\\caption{}\n\\end{figure}`
 * `wsl2ImageTransfer.mvFlag`: 是否将图片剪贴（而非复制）到目标目录，默认为`true`。
@@ -39,3 +39,12 @@ vsce package
 
 添加了对 windows 系统中类似功能的支持。改用了更小的 icon。
 
+### 1.0.2-4
+
+修改了 icon
+
+### 1.0.5
+
+添加了自动搜索截屏保存路径的功能，不需要用户自行配置。
+
+添加了开源证书。
--- extension.js
+++ extension.js
@@ -3,13 +3,77 @@ const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
-function activate(context) {
-    const config = vscode.workspace.getConfiguration('wsl2ImageTransfer');
-    let screenshotDir = config.get('screenshotDir');
-    const insertContent = config.get('insertContent');
-    const mvFlag = config.get('mvFlag');
+const platform = os.platform();
+let config;
+let insertContent;
+let mvFlag;
+let screenshotDir;
 
-    // 获取当前打开文件的目录
+/**
+ * 从注册表中获取 Windows 截图保存路径
+ */
+function getWindowsScreenshotDir() {
+    try {
+        vscode.window.showInformationMessage('发现您还没有配置截图保存路径，正在尝试从注册表中获取\nε=( o｀ω′)ノ');
+        let output = '';
+        switch (platform) {
+            case 'win32':
+                output = execSync('reg query "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" /v "{B7BEDE81-DF94-4682-A7D8-57A52620B86F}"', { encoding: 'utf-8' });
+                break;
+            case 'linux':
+                output = execSync('wsl.exe cmd.exe /c reg query "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" /v "{B7BEDE81-DF94-4682-A7D8-57A52620B86F}"', { encoding: 'utf-8' });
+                break;
+            default:
+                break;
+        }
+        const match = output.match(/\{B7BEDE81-DF94-4682-A7D8-57A52620B86F}\s+REG_EXPAND_SZ\s+(.*)/);
+        let screenshotDir = '';
+        if (match && match[1]) {
+            screenshotDir = match[1].trim();
+            vscode.window.showInformationMessage(`找到您的 Windows 截图保存路径: ${screenshotDir}，已更新到配置文件\no(*￣▽￣*)ブ`);
+            vscode.workspace.getConfiguration().update('wsl2ImageTransfer.screenshotDir', screenshotDir, vscode.ConfigurationTarget.Global);
+            return screenshotDir;
+        }
+        else {
+            vscode.window.showErrorMessage('未能找到您的 Windows 截图保存路径\n/(ㄒoㄒ)/~~');
+        }
+    } catch (err) {
+        console.error('从注册表中获取截图保存路径失败\n/(ㄒoㄒ)/~~:', err);
+        vscode.window.showErrorMessage('从注册表中获取截图保存路径失败');
+    }
+}
+
+/**
+ * 加载配置
+*/
+function loadConfig() {
+    config = vscode.workspace.getConfiguration('wsl2ImageTransfer');
+    insertContent = config.get('insertContent');
+    mvFlag = config.get('mvFlag');
+    screenshotDir = config.get('screenshotDir');
+    if(!screenshotDir) {
+        screenshotDir = getWindowsScreenshotDir();
+    }
+    switch (platform) {
+        case 'win32':
+            // 处理 screenshotDir，确保它是一个 Windows 可以访问的路径
+            screenshotDir = screenshotDir.replace(/\/mnt\/([a-z])\//, (match, p1) => `${p1.toUpperCase()}:\\`).replace(/\//g, '\\');
+            break;
+        case 'linux':
+            // 处理 screenshotDir，确保它是一个 WSL2 可以访问的路径
+            screenshotDir = screenshotDir.replace(/^([A-Za-z]):/, (match, p1) => {
+                return `/mnt/${p1.toLowerCase()}`;
+            }).replace(/\\/g, '/');
+            break;
+        default:
+            break;
+    }
+}
+
+loadConfig();
+
+
+function activate(context) {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
         vscode.window.showErrorMessage('没有活动的编辑器.');
@@ -26,23 +90,8 @@ function activate(context) {
     if (!relativeTargetDir.endsWith('/')) {
         relativeTargetDir += '/';
     }
-    const platform = os.platform();
-    switch (platform) {
-        case 'win32':
-            // 处理 screenshotDir，确保它是一个 Windows 可以访问的路径
-            screenshotDir = screenshotDir.replace(/\/mnt\/([a-z])\//, (match, p1) => `${p1.toUpperCase()}:\\`).replace(/\//g, '\\');
-            break;
-        case 'linux':
-            // 处理 screenshotDir，确保它是一个 WSL2 可以访问的路径
-            screenshotDir = screenshotDir.replace(/^([A-Za-z]):/, (match, p1) => {
-                return `/mnt/${p1.toLowerCase()}`;
-            }).replace(/\\/g, '/');
-            break;
-        default:
-            break;
-    }
 
-    const disposable = vscode.commands.registerCommand('wsl2ImageTransfer.transferImage', function () {
+    const disposable = vscode.commands.registerCommand('wsl2ImageTransfer.transferImage', async function () {
         // 获取 screenshotDir 中创建时间最迟的图片
         const files = fs.readdirSync(screenshotDir);
         let latestFile = null;
@@ -58,7 +107,7 @@ function activate(context) {
         });
 
         if (!latestFile) {
-            vscode.window.showErrorMessage('没有找到最新的截图文件.');
+            vscode.window.showErrorMessage('没有找到最新的截图文件\n(ˉ▽ˉ；)....');
             return;
         }
 
@@ -89,7 +138,6 @@ function activate(context) {
                 }
             }
 
-            const editor = vscode.window.activeTextEditor;
             if (editor) {
                 const contentToInsert = insertContent.replace('${filePath}', './' + relativeTargetDir + latestFile);
                 const snippet = new vscode.SnippetString(contentToInsert);
@@ -97,7 +145,7 @@ function activate(context) {
             }
 
         } catch (err) {
-            vscode.window.showErrorMessage(`复制或剪切图像失败: ${err.message}`);
+            vscode.window.showErrorMessage(`复制或剪切图像失败\n/(ㄒoㄒ)/~~: ${err.message}`);
         }
     });
 
